1198f19f73b74c4f649938b4487927aa4a447dce
Tetiana Voznesenska <tetiana.voznesenska@ukr.net>
2024-05-27 01:02:12 +0300

    REQ-09: Ability to add a few pets per client + some bug fix

--- src/main/java/com/magicvet/service/PetService.java
+++ src/main/java/com/magicvet/service/PetService.java
@@ -34,7 +34,8 @@ public class PetService {
         pet.setSex(Main.SCANNER.nextLine());
         if (type.equals(DOG_TYPE)) {
             System.out.println("Size (XS, S, M, L, XL): ");
-            ((Dog) pet).setSize(Main.SCANNER.nextLine());
+            String size = Main.SCANNER.nextLine();
+            ((Dog) pet).setSize(Dog.Size.valueOf(size));
         }
         return pet;
     }
